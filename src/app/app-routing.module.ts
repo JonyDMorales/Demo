16d2e101096a59ff384f00cb250960bc420707f6
JonyDMorales <jonatan_moralest@hotmail.com>
2019-10-14 17:45:38 -0500

    Avances

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {OperacionesComponent} from './pages/operaciones/operaciones.component';
+import {LoginComponent} from './pages/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'operaciones', component: OperacionesComponent },
+  { path: '**' , pathMatch: 'full' , redirectTo: 'login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OperacionesComponent } from './pages/operaciones/operaciones.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OperacionesComponent,
+    LoginComponent,
+    SidemenuComponent
   ],
   imports: [
     BrowserModule,
